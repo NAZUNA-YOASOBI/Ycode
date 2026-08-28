9573a16aa6c496d14a7f53b459d377e748927241
NAZUNA <2682559367@qq.com>
2026-08-29 07:22:58 +0800

    optimize_block_emerging_timing

--- test/ycode_smoke.py
+++ test/ycode_smoke.py
@@ -1,25 +1,12 @@
-def merge_sort(arr):
+def quick_sort(arr):
     if len(arr) <= 1:
         return arr
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-    return merge(left, right)
-
-def merge(left, right):
-    result = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
 
 if __name__ == "__main__":
     data = [3, 6, 8, 10, 1, 2, 1]
-    print(merge_sort(data))
+    print(quick_sort(data))
